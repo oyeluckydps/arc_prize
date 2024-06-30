c4210b9d54262a11475f2dd3e525d3390a86bfe4
Abhinav Kumar Verma <oyeluckydpsvaranasi@gmail.com>
2024-07-01 01:32:03 +0530

    Add some more images.

--- pythonProject/README.md
+++ pythonProject/README.md
@@ -5,3 +5,19 @@ This project provides a comprehensive solution for the ARC Prize competition, ai
 The core idea behind this project is to leverage both symbolic and neural methods to create a hybrid approach for solving ARC tasks. By combining discrete program search with deep learning-driven intuition, our solution navigates the vast program space more efficiently. This approach mimics human cognitive processes, using symbolic reasoning for step-by-step problem solving while employing neural networks to intuitively prune the search space. Additionally, we utilize techniques such as domain-specific language (DSL) program synthesis and active inference to further enhance our model's ability to generalize from limited training data.
 
 The ARC Prize competition is not just a test of technical prowess but a step towards understanding and replicating human-like intelligence in machines. The competition underscores the importance of skill acquisition and generalization, with a stringent evaluation process to ensure fairness and rigor. By participating in this challenge, our project aims to contribute to the broader AI research community, pushing the boundaries of what AI systems can achieve and bringing us closer to the realization of true AGI.
+
+## Must DO:
+
+You must comment the section of the code in the Pydantic BaseModel class in the crate_model class. Refer to this issue for more details: https://github.com/pydantic/pydantic/discussions/5782. Hopefully, this will be fixed in a future version of Pydantic.
+    
+    if __base__ is not None:
+        if __config__ is not None:
+            pass
+            # raise PydanticUserError(
+            #     'to avoid confusion `__config__` and `__base__` cannot be used together',
+            #     code='create-model-config-base',
+            # )
+        if not isinstance(__base__, tuple):
+            __base__ = (__base__,)
+
+
--- pythonProject/llm/connectors/tests/test_dspy.py
+++ pythonProject/llm/connectors/tests/test_dspy.py
@@ -3,6 +3,8 @@ import os, sys
 import dspy
 from pydantic import BaseModel, Field
 from typing import List
+from PIL import Image, JpegImagePlugin
+from pathlib import Path
 
 # Adjust the Python path to include the parent package
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
@@ -34,6 +36,23 @@ his mind often lost in the precision of ticking gears and philosophical musings.
 Completing the trio was Emilia, the empathetic and gentle schoolteacher,
 whose nurturing nature and soothing voice calmed even the rowdiest classroom.
 """
+
+dspy.settings.configure(arbitrary_types_allowed=True)
+
+class GridAnalysisOutput(BaseModel):
+    num_rows: int
+    num_columns: int
+    colors: List[str]  # List of color strings in hexadecimal format
+
+# Define the Signature for the image to grid analysis task.
+class ImageAnalysis(dspy.Signature, BaseModel):
+    '''
+    Given the image, I want you to analyze the number of rows and columns in the grid, and the colors present in the grid as a list of human readable color names.
+    '''
+    image: JpegImagePlugin.JpegImageFile = dspy.InputField(desc="The input image containing the grid.")
+    analysis: GridAnalysisOutput = dspy.OutputField(desc="Number of rows and columns in the grid, and the colors present in the grid as a list of human readable color names.")
+    class Config:
+        arbitrary_types_allowed = True
 
 
 class TestDSPy(unittest.TestCase):
@@ -72,6 +91,16 @@ class TestDSPy(unittest.TestCase):
         self.assertIsNotNone(response)
         self.assertIsInstance(response.characters, list)
         self.assertEqual(len(response.characters), 3)
+
+    def test_one_shot_with_image(self):
+        dspy_instance = DSPy(strategy_method='one_shot', system_info='', io_signature=ImageAnalysis)
+        img = Image.open(Path("./spiral.jpg"))
+        response = dspy_instance.send_message(image = img)
+        self.assertIsNotNone(response)
+        self.assertIsInstance(response.analysis, GridAnalysisOutput)
+        self.assertEqual(response.analysis.num_rows, 11)
+        self.assertEqual(response.analysis.num_columns, 11)
+        self.assertEqual(len(response.analysis.colors), 4)
 
 if __name__ == '__main__':
     unittest.main()
